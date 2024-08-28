e640f4e63125f7e073a05a4c0994cf54a8621105
lilyprice24 <lilyprice@unomaha.edu>
2024-08-28 16:40:37 +0000

    first update

--- FirstProgram.py
+++ FirstProgram.py
@@ -4,18 +4,20 @@
 #Assignment:
 
 def main():
-  print("First Program")
+  #print("First Program")
   #Say hello
-  
+  print("Hello!")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("It is nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("How old are you? ")
   #Tell the user what year they were born in.
+  age = int(age)
+  birthYear = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
+  print("You were born in", birthYear)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -6,9 +6,14 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("Enter a noun: ")
+  verb1 = input("Enter a verb: ")
+  adjective1 = input("Enter an adjective: ")
+  noun2 = input("Enter a noun: ")
+  verb2 = input("Enter a verb: ")
+  adjective2 = input("Enter an adjective: ")
   #Print the story with the user supplied words.
-
+  print("I like to walk my " + noun1 + " and " + noun2 + " during the " + adjective1 + " night. " )
 
 
 #Call the main function if this is the file being run.
